f56f6670917b58046a0af5d618e44636c2970d98
sthyagarajan <sunder.thyagarajan@sjsu.edu>
2018-05-05 12:31:17 -0700

    Demo JX Springboot

---
+++ src/main/java/com/employee365/employee365portal/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.employee365.employee365portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
